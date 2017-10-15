dd30467b1aae2c27dbe5b8ba1d373e478f041b58
jazmin10 <jazminestrada10@gmail.com>
2017-10-15 17:03:44 -0500

    added navbar, which collapses during window scroll

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -10,8 +10,41 @@ $(document).ready(function(){
 	function fadeIn(){
 		$(".fade-in").fadeIn(7000);
 	}
-	
+
+	function collapseNavbar() {
+    if ($(".navbar").offset().top > 100) {
+        $(".navbar-header").css("float", "none");
+        $(".navbar-toggle").css("display", "block");
+        $(".navbar-nav").css("float", "none!important");
+        $(".navbar-nav>li").css("float", "none");
+        $("#bs-example-navbar-collapse-1").addClass("scroll-navbar");
+   
+    } else {
+        $(".navbar-header").removeAttr("style");
+        $(".navbar-toggle").removeAttr("style");
+        $(".navbar-nav").removeAttr("style");
+        $(".navbar-nav>li").removeAttr("style");
+        $("#bs-example-navbar-collapse-1").removeClass("scroll-navbar");
+    }
+	}
+
 	setTimeout(intro, 500);
 	setTimeout(fadeIn, 3000);
+	$(window).scroll(collapseNavbar);
+
+	$("#toggle-menu").on("click", function(){
+		var toggleValue = $("#toggle-menu").attr("value");
+
+		if (toggleValue === "true") {
+			$(".navbar-header").removeClass("spread-menu");
+			$("#toggle-menu").attr("value", "false");
+		}
+		else {
+			$(".navbar-header").addClass("spread-menu");
+			$("#toggle-menu").attr("value", "true");
+		}
+	});
+
+
 	
 });
